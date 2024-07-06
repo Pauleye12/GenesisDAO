7cfbe10e3737f8899212a9928d2fe045440db791
pauleye <orepaul12@gmail.com>
2024-07-06 15:27:49 +0100

    almost done with herosection

--- src/Components/Herosection/HeroSection.tsx
+++ src/Components/Herosection/HeroSection.tsx
@@ -14,6 +14,19 @@ const HeroSection = () => {
         src="./images/heroright.png"
         alt=""
       />
+      <div className="hero-section__content1">
+        <h3>Welcome to Genesis DAO</h3>
+        <h1>Explore the essence of Genesis DAO</h1>
+        <p>
+          Connect your wallets and secure your crypto assets with the unique and
+          true decentralised organisation.
+        </p>
+        <a href="#">Enter App</a>
+      </div>
+      <div className="hero-section__content2">
+        <img src="./images/heroCenterimg.png" alt="" />
+      </div>
+      <div className="hero-section__content3"></div>
     </div>
   );
 };
